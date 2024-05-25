a6471ff2e9b74c7c591a731ca96565f1d89e4c40
Edem Ablayev <eablaev3@gmail.com>
2024-05-25 23:24:00 +0300

    init commit

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,18 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData(){
+        String text = "Hello";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo(text));
+    }
+}
